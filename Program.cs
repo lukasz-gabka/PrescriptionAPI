7cdd45787c8686a047582cd5cd1218aafdb03c9a
lukasz-gabka <lukasz.gabka.programista@gmail.com>
2022-02-10 18:50:52 +0100

    added dbContext

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using PrescriptionAPI.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ApiDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
