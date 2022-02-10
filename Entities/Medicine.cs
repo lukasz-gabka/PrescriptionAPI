5aa882c82c26d768a37cf1576a1112a983e30c30
lukasz-gabka <lukasz.gabka.programista@gmail.com>
2022-02-10 20:22:07 +0100

    added entities

---
+++ Entities/Medicine.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PrescriptionAPI.Entities;
+
+public class Medicine
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Dosage { get; set; }
+    public int TreatmentTime { get; set; }
+    public string? Comment { get; set; }
+    public DateTime StartDate { get; set; }
+    public int? FirstDayException { get; set; }
+    public int? LastDayException { get; set; }
+    public string DosageString { get; set; }
+    public int PatientId { get; set; }
+    [JsonIgnore]
+    public virtual Patient Patient { get; set; }
+}
---
+++ Entities/Patient.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PrescriptionAPI.Entities;
+
+public class Patient
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Symptoms { get; set; }
+    public virtual IEnumerable<Medicine> Medicines { get; set; }
+    public int PrescriptionId { get; set; }
+    [JsonIgnore]
+    public virtual Prescription Prescription { get; set; }
+}
