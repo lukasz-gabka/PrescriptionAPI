079ac5a22e525ee7a8b46649d14e5b692e2cd0f9
lukasz-gabka <lukasz.gabka.programista@gmail.com>
2022-02-13 15:37:13 +0100

    added maxlength to string columns in database

--- Entities/Medicine.cs
+++ Entities/Medicine.cs
@@ -1,13 +1,16 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace PrescriptionAPI.Entities;
 
 public class Medicine
 {
     public int Id { get; set; }
+    [StringLength(50)]
     public string Name { get; set; }
     public int Dosage { get; set; }
     public int TreatmentTime { get; set; }
+    [StringLength(300)]
     public string? Comment { get; set; }
     public DateTime StartDate { get; set; }
     public int? FirstDayException { get; set; }
